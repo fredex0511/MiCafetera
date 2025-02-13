60a35e32027ee3792491286c623356fd904a7cf3
fredex0511 <elcholobrizuelamail.com>
2025-02-13 13:18:41 -0600

    [UPD] Browserouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from "./components/LandingPage";
 import AboutUs from "./components/AboutUs";
 import CoffeeTypes from "./components/CoffeeTypes";
